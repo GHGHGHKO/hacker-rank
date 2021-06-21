cdf205cc0293d66f089bb6d5626bd944dfcac20c
GHGHGHKO <gudrb963@gmail.com>
2021-06-22 03:37:21 +0900

    30Days

---
+++ Java/30Days/Day_4_Class_vs_Instance.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Day_4_Class_vs_Instance {
+    private int age;	
+  
+	public Day_4_Class_vs_Instance(int initialAge) {
+  		// Add some more code to run some checks on initialAge
+	this.age = initialAge;
+    }
+
+    public void amIOld() {
+          // Write code determining if this Day_4_Class_vs_Instance's age is old and print the correct statement:
+        // System.out.println(/*Insert correct print statement here*/);
+        if (age < 0) {
+            System.out.println("Age is not valid, setting age to 0.");
+            age = 0;
+        }
+        if (age < 13) {
+            System.out.println("You are young.");
+        } else if(age >= 13 && age < 18) {
+            System.out.println("You are a teenager.");
+        } else {
+            System.out.println("You are old.");
+        }
+    }
+
+    public void yearPasses() {
+          // Increment this Day_4_Class_vs_Instance's age.
+          age += 1;
+    }
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int age = sc.nextInt();
+			Day_4_Class_vs_Instance p = new Day_4_Class_vs_Instance(age);
+			p.amIOld();
+			for (int j = 0; j < 3; j++) {
+				p.yearPasses();
+			}
+			p.amIOld();
+			System.out.println();
+        }
+		sc.close();
+    }
+}
